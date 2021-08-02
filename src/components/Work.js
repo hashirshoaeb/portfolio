2b1ee6d48955aea7ee81f2bf285ebaab3aa7186e
hashirshoaeb <hashirshoaeb@gmail.com>
2021-08-02 20:17:06 +0500

    work components

---
+++ src/components/Work.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
+
+export const Skills = ({ title, cards }) => {
+	return (
+		<div id="skills" className="bg-secondary py-5 px-5">
+			<div className="container">
+				<h1 className="text-primary fw-bold">{title}</h1>
+				<div className="d-flex flex-row flex-wrap justify-content-center">
+					{cards.map((value, index) => (
+						<Card key={index} title={value.title} description={value.description} link={value.link} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export const Projects = ({ title, cards }) => {
+	return (
+		<div id="projects" className="bg-primary py-5 px-5">
+			<div className="container">
+				<h1 className="text-light fw-bold">Projects</h1>
+				<div className="d-flex flex-row flex-wrap justify-content-center">
+					{cards.map((value, index) => (
+						<Card key={index} title={value.title} description={value.description} icons={value.icons} />
+					))}
+				</div>
+				{/* <div className="text-center">
+					<button type="button" class="btn btn-outline-light">See More</button>
+				</div> */}
+			</div>
+		</div>
+	);
+}
+
+export const Card = ({ title, description, icons }) => {
+	return (
+		<div className="card py-3 px-3 mx-sm-4 my-4 card-work" style={{ width: "20rem" }}>
+			<h4 className="text-primary">{title}</h4>
+			<p className="text-dark">{description}</p>
+			<div className="text-end">
+				{icons && icons.map((value, index) => (
+					<a key={index} href={value.link} target="_blank" rel="noreferrer">
+						<FontAwesomeIcon className="icon-style mx-1" icon={value.icon} size="2x" />
+					</a>
+				))}
+			</div>
+		</div>
+	);
+}
