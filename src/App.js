930c8b2fe0edad45761bf2790975f66f4fcde084
hashirshoaeb <hashirshoaeb@gmail.com>
2021-08-02 20:18:46 +0500

    using all of the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Nav } from './components/Navbar';
+import { Intro, About } from './components/Intro';
+import { Skills, Projects } from './components/Work';
+import { Footer, Contact } from './components/Footer';
+import { about, contact, intro, navigation, projects, work } from './config/config'
 
-function App() {
+export const App = () => {
   return (
-    <div >
+    <div>
+      <Nav
+        title={navigation.name}
+        links={navigation.links}
+      />
+      <Intro
+        title={intro.title}
+        description={intro.description}
+        image={intro.image}
+        buttons={intro.buttons}
+      />
+      <About
+        title={about.title}
+        description={about.description}
+      />
+      <Skills
+        title={work.title}
+        cards={work.cards}
+      />
+      <Projects
+        title={projects.title}
+        cards={projects.cards}
+      />
+      <Contact
+        title={contact.title}
+        description={contact.description}
+        buttons={contact.buttons}
+      />
+      <Footer />
     </div>
   );
 }
-
-export default App;
