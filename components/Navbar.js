3cf6c75a8175981249af3fb21d2046b0ada9add6
hashirshoaeb <hashirshoaeb@gmail.com>
2021-08-05 22:13:11 +0500

    migration from reactjs to nextjs

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,17 +7,17 @@ export const Nav = ({ title, links }) => {
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   return (
-    <nav class="navbar navbar-expand-sm navbar-light bg-secondary">
+    <nav className="navbar navbar-expand-sm navbar-light bg-secondary">
       <div className="container">
-        <a class="navbar-brand" href="/">
+        <a className="navbar-brand" href="/">
           {/* <img src={Logo} alt="Logo" width="36" height="36" className="vertical-align-middle" /> */}
           <span className="">{title}</span>
         </a>
-        <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
-          <span class="navbar-toggler-icon"></span>
+        <button className="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarsExample09">
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarsExample09">
           {links.map((value, index) => (
             <a key={index} className="nav-link" href={value.link}>{value.title}</a>
           ))}
