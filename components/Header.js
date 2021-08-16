0c25aa7627d3baa935f086ba9978e741bec16a8b
hashirshoaeb <hashirshoaeb@gmail.com>
2021-08-16 21:35:18 +0500

    seo settings

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export const Header = ({ seo }) => {
+  return (
+    <Head>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:type" content="website" />
+    </Head>
+  )
+}
