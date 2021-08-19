5da2d2ac03cef7e9d9c1526c7fe7740c662c7470
hashirshoaeb <hashirshoaeb@gmail.com>
2021-08-19 08:06:37 +0500

    links added

---
+++ pages/links.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import Link from 'next/link';
+import { Footer } from '../components/Footer';
+import { links, SEO, } from '../config/config';
+import { Header } from '../components/Header';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Header seo={SEO} />
+      <div className="d-flex flex-column justify-content-between bg-secondary min-vh-100">
+        <div className="py-5 px-5 container text-center">
+          <img className="img-fluid my-3 card-image" width="150" height="150" src={links.image} alt="profile of hashirshoaeb" />
+          <h3 className="mt-3">{links.title}</h3>
+          <p>{links.description}</p>
+          {links.cards.map((value, index) => (
+            <Button key={index} title={value.title} link={value.link} />
+          ))}
+        </div>
+        <Footer />
+      </div>
+    </Fragment>
+  );
+}
+
+
+function Button({ title, link }) {
+  return (
+    <Link href={link}>
+      <a target="_blank" rel="noreferrer">
+        <div className="row justify-content-center">
+          <div className="card py-3 px-3 mx-sm-4 my-2 card-work" style={{ width: "20rem" }}>
+            <h4 className="text-primary">{title}</h4>
+          </div>
+        </div>
+      </a>
+    </Link>);
+}
