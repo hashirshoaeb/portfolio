5cc41e6bd880642a729bdf482763a6bb696fb81c
hashirshoaeb <hashirshoaeb@gmail.com>
2021-08-19 22:18:46 +0500

    test

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const baseURL = "/portfolio";
 module.exports = {
   reactStrictMode: true,
   basePath: process.env.BASE_PATH ?? baseURL,
-  // assetPrefix: process.env.BASE_PATH ?? baseURL,
+  assetPrefix: process.env.BASE_PATH ?? baseURL,
 }
