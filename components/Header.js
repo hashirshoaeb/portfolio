4a91e53ce7933a5692f33a911b956aada678d9da
hashirshoaeb <hashirshoaeb@gmail.com>
2021-08-24 07:12:27 +0500

    bug fixes

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,6 @@ export const Header = ({ seo }) => {
     <Head>
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
-      <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
