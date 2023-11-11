0f05da9b93927c27487aaf228c0402b5e1e283e9
hashirshoaeb <hashirshoaeb@gmail.com>
2023-11-11 12:03:01 +0500

    blog link added

--- config/config.js
+++ config/config.js
@@ -22,6 +22,10 @@ export const navigation = {
 			title: "Links",
 			link: "/links",
 		},
+		{
+			title: "Blog",
+			link: "https://medium.com/@hashirshoaeb",
+		}
 	],
 }
 export const intro = {
